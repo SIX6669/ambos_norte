7bbfbd00aa9d17c8a9108ca481c9f122993fba93
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2025-11-04 19:39:32 -0300

    arreglo-admin-usuario

--- ambos_norte_project/apps/usuarios/admin.py
+++ ambos_norte_project/apps/usuarios/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario, Direccion
 
-# Register your models here.
+class DireccionInline(admin.TabularInline):
+    model = Direccion
+    extra = 0
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ['username', 'email', 'tipo_usuario', 'telefono', 'is_active']
+    list_filter = ['tipo_usuario', 'is_active', 'is_staff']
+    search_fields = ['username', 'email', 'telefono']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información Adicional', {
+            'fields': ('telefono', 'tipo_usuario')
+        }),
+    )
+    
+    inlines = [DireccionInline]
+
+@admin.register(Direccion)
+class DireccionAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'calle', 'numero', 'ciudad', 'provincia', 'es_predeterminada']
+    list_filter = ['provincia', 'ciudad', 'es_predeterminada']
+    search_fields = ['usuario__username', 'calle', 'ciudad']
