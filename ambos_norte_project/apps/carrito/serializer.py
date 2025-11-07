46c211157210baacaa2d572648d00e8a989f972a
Six_mother <simon.494.perez@gmail.com>
2025-11-07 19:22:09 -0300

    arregle los serializer, views de carrito

--- ambos_norte_project/apps/carrito/serializer.py
+++ ambos_norte_project/apps/carrito/serializer.py
@@ -1,14 +1,33 @@
 from rest_framework import serializers
 from .models import Carrito, ItemCarrito
+from ..catalogo.serilizer import ProductoSerializer
+
+class ItemCarritoSerializer(serializers.ModelSerializer):
+    
+    producto = ProductoSerializer(read_only='True')
+    subtotal = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ItemCarrito
+        fields = '__all__'
+        read_only = ('fecha_agregado',)
+    
+    def get_subtotal(self, obj):
+        return obj.subtotal()
 
 class CarritoSerializer(serializers.ModelSerializer):
+    
+    items = ItemCarritoSerializer(many=True, read_only=True)
+    subtotal = serializers.SerializerMethodField()
+    total_items = serializers.SerializerMethodField()
+
     class Meta:
         model = Carrito
         fields = '__all__'
         read_only = ('fecha_creacion','fecha_modificacion',)
 
-class ItemCarritoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ItemCarrito
-        fields = '__all__'
-        read_only = ('fecha_agregado',)
+    def get_subtotal(self, obj):
+        return obj.calcular_subtotal()
+
+    def get_total_items(self, obj):
+        return obj.total_items()
--- ambos_norte_project/apps/carrito/views.py
+++ ambos_norte_project/apps/carrito/views.py
@@ -1,11 +1,45 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import ValidationError 
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Carrito, ItemCarrito
+from ..catalogo.models import Producto
 from .serializer import CarritoSerializer,ItemCarritoSerializer
 
 class CarritoViewSet(viewsets.ModelViewSet):
     queryset = Carrito.objects.all()
     serializer_class = CarritoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
+    
+    @action(default =True, methods=['post'])
+    def agregar_item(self,request, pk=None):
+        carrito = self.get_object()
+        producto_id = request.data.get('producto_id')
+        cantidad = int(request.data.get('cantidad',1))
+        producto = get_object_or_404(Producto, pk=producto_id)
+
+        try:
+            item, creado = ItemCarrito.objects.get_or_create(
+                carrito=carrito,
+                producto = producto,
+                defaults={'cantidad': cantidad, 'precio_unitario':producto.precio}  
+            )
+
+            if not creado:
+                item.cantidad += cantidad
+                item.save()
+            
+            return Response(ItemCarritoSerializer(item).data, status=statu.HTTP_201_CREATED)
+        except:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def vaciar(self, request, pk=None):
+        carrito = self.get_object()
+        carrito.item,all().delete()
+        return Response({'mensaje': 'Carrito vaciado correctamente.'}, status=status.HTTP_200_OK)
 
 class ItemCarritoViewSet(viewsets.ModelViewSet):
     queryset = ItemCarrito.objects.all()
